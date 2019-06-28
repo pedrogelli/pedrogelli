9b1591a58b3affa584a4b41a9e26236ae6313558
Pedro Gelli <pedrogelli@gmail.com>
2019-06-28 12:54:40 -0300

    Initial Layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container contato">
+    <div class="row">
+      <div class="col-12">
+        <a
+          href="mailto:pedrogelli@gmail.com"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="pedrogelli@gmail.com"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#email"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="tel:+5521969400215"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="+55 21 96940-0215"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#phone"
+            ></use>
+          </svg>
+        </a>
+        <a href="mailto:pedrogelli@gmail.com" class="email-text"
+          >pedrogelli@gmail.com</a
+        >
+        <p>Rio de Janeiro, RJ - Brasil</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/Links.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="container links">
+    <div class="row social">
+      <div class="col">
+        <a
+          href="https://www.behance.net/pedrogelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Behance"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#behance"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://www.deviantart.com/pedrogelli/gallery/"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="DeviantArt"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#deviantart"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://github.com/pedrogelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="GitHub"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#github"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://pt.stackoverflow.com/users/2091/pedro-gelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Stack OverFlow"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#stackoverflow"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://youtube.com/channel/UCV1DK9uMlCOTqJLTkXNYinQ"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Youtube"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#youtube"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://vimeo.com/pedrogelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Vimeo"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#vimeo"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://soundcloud.com/pedro-gelli-tinoco"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="SoundCloud"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#soundcloud"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://linkedin.com/pedro-gelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="LinkedIn"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#linkedin"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://twitter.com/pedrogelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Twitter"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#twitter"
+            ></use>
+          </svg>
+        </a>
+        <a
+          href="https://instagram.com/pedrogelli"
+          target="_blank"
+          class="link"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Instagram"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#instagram"
+            ></use>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="row tools">
+      <div class="col">
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#arduino"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#jquery"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#vuejs"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#bootstrap"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#html"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#css"></use>
+        </svg>
+        <svg class="icon">
+          <use xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#js"></use>
+        </svg>
+        <svg class="icon">
+          <use xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#php"></use>
+        </svg>
+        <svg class="icon">
+          <use xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#git"></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#windows"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#apple"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#adobe"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#linux"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#figma"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#3dsmax"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#ableton"
+          ></use>
+        </svg>
+        <svg class="icon">
+          <use
+            xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#terragen"
+          ></use>
+        </svg>
+      </div>
+    </div>
+    <div class="row curriculo">
+      <div class="col">
+        <a
+          href="#"
+          target="_blank"
+          class="mx-auto"
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Currículo.pdf"
+        >
+          <svg class="icon">
+            <use
+              xlink:href="@/assets/pedrogelli-portifolio-sprite.svg#file-pdf-regular"
+            ></use>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Links"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
